370bb2f4bab82a3e9fa502d731b1ac1f319221c8
rafael25 <rafaelvb25@gmail.com>
2014-12-01 14:50:00 -0600

    Eager loading de Recetas.Categorias

--- src/main/java/recetas/backend/model/Recetas.java
+++ src/main/java/recetas/backend/model/Recetas.java
@@ -11,6 +11,7 @@ import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -47,7 +48,7 @@ public class Recetas implements Serializable {
 	private Integer tiempoPrep;
 	@Column(name = "rendimiento")
 	private String rendimiento;
-	@ManyToMany(mappedBy = "recetasList")
+	@ManyToMany(fetch = FetchType.EAGER, mappedBy = "recetasList")
 	@JsonIgnore
 	private List<Categorias> categoriasList;
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "idReceta")
