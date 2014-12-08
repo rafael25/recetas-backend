82a35a3d2e5e48dab5d573a0dc57cad4655b7a81
rafael25 <rafaelvb25@gmail.com>
2014-12-08 03:05:00 -0600

    Add create recetas controller

--- src/main/java/recetas/backend/controller/RecetasController.java
+++ src/main/java/recetas/backend/controller/RecetasController.java
@@ -6,11 +6,15 @@
 package recetas.backend.controller;
 
 import java.io.IOException;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import recetas.backend.json.Receta;
 import recetas.backend.model.DAORecetas;
 
 /**
@@ -31,5 +35,13 @@ public class RecetasController {
     public @ResponseBody String show(@PathVariable Integer id) throws IOException {
 		DAORecetas dao = new DAORecetas();
 		return dao.buscar(id);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, headers = {"Accept=Application/json;Content-Type:Application/json; charset=utf-8"})
+	public @ResponseBody String save(@RequestBody String data) throws IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode jnode = mapper.readValue(data, JsonNode.class);
+		Receta r = mapper.readValue(jnode.get("receta"), Receta.class);
+		return r.toString();
 	}
 }
